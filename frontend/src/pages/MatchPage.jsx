7267107395e4e4a2b36c181660bee743dbb3bd38
Rémy Bernardin <remy.bernardin.sen@gmail.com>
2022-11-03 15:26:31 +0100

    spt2_calendar_add calendar on Click + calendar.css

--- frontend/src/pages/MatchPage.jsx
+++ frontend/src/pages/MatchPage.jsx
@@ -1,27 +1,51 @@
 import "./MatchPage.css";
 import MatchCards from "@components/MatchCards";
+import { useState } from "react";
 import InsideCard from "../img/mobile/inside-card.png";
 import OutsideCard from "../img/mobile/outside-card.png";
 import MatchCardsInfos from "../data/MatchCardsInfos";
+import Calendar from "../components/Calendar";
 
 export default function MainPage() {
+  const [viewCalendar, setViewCalendar] = useState(false);
+
   return (
     <section className="match-page">
       <div className="logo-container">
-        <img className="logo" src="src/img/mobile/logoGetMatch.png" alt="logo" />
+        <img
+          className="logo"
+          src="src/img/mobile/logoGetMatch.png"
+          alt="logo"
+        />
       </div>
-      <div className="hashtag-bar">
-      </div>
+      <div className="hashtag-bar" />
       <div className="search-buttons">
         <div className="schedule">
-          <img className="icons" src="src/img/icons/schedule-white.png" alt="schedule-icons"/>
+          <img
+            className="icons"
+            src="src/img/icons/schedule-white.png"
+            alt="schedule-icons"
+          />
           <p>08:00 AM</p>
         </div>
-        <div className="calendar">
-        <img className="icons" src="src/img/icons/calendar-white.png" alt="calendar-icons"/>
+        <div
+          onClick={() => setViewCalendar(true)}
+          onKeyDown={() => setViewCalendar(true)}
+          className="calendar"
+          role="button"
+          tabIndex={0}
+        >
+          <img
+            className="icons"
+            src="src/img/icons/calendar-white.png"
+            alt="calendar-icons"
+          />
           <p>08/11/2024</p>
         </div>
-        
+        <Calendar
+          viewCalendar={viewCalendar}
+          setViewCalendar={setViewCalendar}
+        />
       </div>
       <div className="cards-container">
         {MatchCardsInfos.map((element, index) => (
