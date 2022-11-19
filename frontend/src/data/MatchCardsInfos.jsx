fe5a70fe084404cc312f9ba6cfd802c44637e9b9
Enhgine <leon.versavel@gmail.com>
2022-11-19 13:40:40 +0100

    add card with 5vs5 and 1vs1

--- frontend/src/data/MatchCardsInfos.jsx
+++ frontend/src/data/MatchCardsInfos.jsx
@@ -66,6 +66,138 @@ const MatchCardsInfos = [
     playersLeft: 2,
     groundType: "Inside",
   },
+  {
+    id: 2,
+    city: "NEW-YORK",
+    adress: "31000 Toulouse 52 rue des blanchers",
+    date: "12/12/2022",
+    time: "09:00 AM",
+    versus: "1vs1",
+    maxPlayers: 2,
+    team1: [
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+    ],
+    team2: [
+      {
+        id: 4,
+        name: "Francis",
+        age: "12",
+        from: "Paris",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1628389.png",
+        isOpen: false,
+      },
+    ],
+    playersLeft: 1,
+    groundType: "Outside",
+  },
+  {
+    id: 3,
+    city: "NEW-YORK",
+    adress: "31000 Toulouse 52 rue des blanchers",
+    date: "12/12/2022",
+    time: "08:00 AM",
+    versus: "5vs5",
+    maxPlayers: 10,
+    team1: [
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+      {
+        id: 2,
+        name: "Bernard",
+        age: "122",
+        from: "Paris",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1628389.png",
+        isOpen: false,
+      },
+      {
+        id: 3,
+        name: "Jean",
+        age: "12",
+        from: "Perpignan",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203500.png",
+        isOpen: false,
+      },
+      {
+        id: 4,
+        name: "Jean",
+        age: "12",
+        from: "Perpignan",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203500.png",
+        isOpen: false,
+      },
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+    ],
+    team2: [
+      {
+        id: 6,
+        name: "Francis",
+        age: "12",
+        from: "Paris",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/1628389.png",
+        isOpen: false,
+      },
+      {
+        id: 7,
+        name: "Kevin",
+        age: "12",
+        from: "Paris",
+        avatar:
+          "https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/203500.png",
+        isOpen: false,
+      },
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+      {
+        id: null,
+        name: null,
+        age: null,
+        from: null,
+        avatar: null,
+        isOpen: true,
+      },
+    ],
+    playersLeft: 5,
+    groundType: "Inside",
+  },
 ];
 
 export default MatchCardsInfos;
